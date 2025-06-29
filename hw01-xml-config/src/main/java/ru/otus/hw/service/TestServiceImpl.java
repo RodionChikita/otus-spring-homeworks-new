7646506131d233864ac532793ddc051cf2cbc7e6
RodionChikita <chikita.rodion@gmail.com>
2025-06-29 16:14:05 +0200

    [FEAT] Добавлен функционал вывода тестовых вопросов с вариантами ответа с конфигурированием через xml

---
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,30 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+    private final QuestionDao questionDao;
+
+    @Override
+    public void executeTest() {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+        List<Question> questions = questionDao.findAll();
+        for(int i = 0; questions.size() > i; i++){
+            ioService.printFormattedLine(i+1 + ". %s", questions.get(i).text());
+            List<Answer> answers = questions.get(i).answers();
+            for (int j = 0; answers.size() > j; j++){
+                ioService.printFormattedLine(" " + (j + 1) + ") %s", answers.get(j).text());
+            }
+        }
+        // Получить вопросы из дао и вывести их с вариантами ответов
+    }
+}
